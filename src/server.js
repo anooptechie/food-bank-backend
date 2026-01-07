8a69b91d6b7ebe770725eb6053b20e3b092fd260
anooptechie <anoophubgit@gmail.com.com>
2026-01-07 19:24:26 +0530

    Connect MongoDB using Mongoose

--- src/server.js
+++ src/server.js
@@ -2,6 +2,9 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const app = require("./app");
+const connectDB = require("./config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
