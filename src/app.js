1bb483e2cd248bbb68974af03e31ee36f637d429
anooptechie <anoophubgit@gmail.com.com>
2026-03-23 16:13:54 +0530

    Uff

--- src/app.js
+++ src/app.js
@@ -3,23 +3,23 @@ const inventoryRoutes = require("./routes/inventoryRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
 const globalErrorHandler = require("./controllers/errorController");
-const cors = require("cors")
+const cors = require("cors");
 
 // --- SWAGGER ADDITIONS START ---
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const path = require('path');
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const path = require("path");
 
 // Load the YAML file you created in Step 2
 // This moves UP one directory and then into docs
-const swaggerDocument = YAML.load(path.join(__dirname, '../docs/swagger.yaml'));
+const swaggerDocument = YAML.load(path.join(__dirname, "../docs/swagger.yaml"));
 // --- SWAGGER ADDITIONS END ---
 
 const app = express();
 
 // Middleware to parse JSON
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
 // Swagger UI
 app.get("/health", (req, res) => {
@@ -35,7 +35,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use("/api/auth", authRoutes);
 app.use("/api/inventory", inventoryRoutes); //route mounting
 app.use("/api/users", userRoutes);
-
 
 // Test route
 app.get("/", (req, res) => {
